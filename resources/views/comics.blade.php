d2ecf1d3bf891e6bb883dd8a20b11cd6a5855df7
leonardoolia <leonardooliaa@gmail.com>
2024-03-13 16:27:55 +0100

    created comics view

---
+++ resources/views/comics.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main')
+
+@section('title', 'Comics')
+
+@section('main-content')
+
+<ul>
+    <li>
+        <div class="card">
+            <img src="" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+            </div>
+        </div>
+    </li>
+</ul>
+
+@endsection
