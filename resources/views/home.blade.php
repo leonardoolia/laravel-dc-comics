7d040e5947940cc1d6f9f1a48f9b185078c3a356
leonardoolia <leonardooliaa@gmail.com>
2024-03-13 16:29:38 +0100

    fixed home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title></title>
+@section('title', 'Home')
 
-</head>
+@section('main-content')
 
-<body>
-    @extends('layouts.main')
-</body>
+<h1>DC Comics Home</h1>
 
-</html>
+@endsection
